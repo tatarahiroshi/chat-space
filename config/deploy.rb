267aed79be8a02699dc50d76b568270728094227
tatarahiroshi <hiroshi.tatara@gmail.com>
2019-11-24 16:50:01 +0900

    syysei

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-sapce'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:tatarahiroshi/chat-space.git'
